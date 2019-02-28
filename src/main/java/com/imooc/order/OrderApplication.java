d6576a24e5239796012644013997748b89e589c4
fanjia1024 <851537449@qq.com>
2019-02-28 23:31:47 +0800

    02.28最后一次版本提交

--- src/main/java/com/imooc/order/OrderApplication.java
+++ src/main/java/com/imooc/order/OrderApplication.java
@@ -3,11 +3,14 @@ package com.imooc.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@ComponentScan(basePackages = "com.imooc")
+@EnableFeignClients(basePackages = {"com.imooc.order.client"})
 public class OrderApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/imooc/order/controller/ClientController.java
+++ src/main/java/com/imooc/order/controller/ClientController.java
@@ -1,10 +1,15 @@
 package com.imooc.order.controller;
 
 import com.imooc.order.client.ProductClient;
+import com.imooc.order.dataobject.ProductInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -33,5 +38,12 @@ public class ClientController {
         String response=productClient.productMsg();
         log.info(response);
         return response;
+    }
+
+    @GetMapping("/getProductList")
+    public String getProductList(){
+        List<ProductInfo> list=productClient.listForOrder(Arrays.asList("164103465734242707"));
+        log.info(list.toString());
+        return list.toString();
     }
 }
