1afec362a70477e62a4f2a14224fd368669bdf83
fanjia1024 <851537449@qq.com>
2019-02-28 20:14:40 +0800

    订单初始化服务代码提交业务代码熟悉直接粘贴

---
+++ src/main/java/com/imooc/order/controller/ClientController.java
@@ -0,0 +1,33 @@
+package com.imooc.order.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class ClientController {
+//    @Autowired
+//    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("getProductMsg")
+    public String getProductMsg(){
+        //1.第一种方式RestTemplate url写死
+//        RestTemplate restTemplate=new RestTemplate();
+//        String response=restTemplate.getForObject("http://localhost:8082/msg",String.class);
+        //2.第二种方式应用loadBalancerClient通过应用名和端口在进行restTemplate
+//        ServiceInstance serviceInstance =loadBalancerClient.choose("PRODUCT");
+//
+//        String url=String.format("http://%s:%s",serviceInstance.getHost(),serviceInstance.getPort())+"/msg";
+//        RestTemplate restTemplate=new RestTemplate();
+//        String response=restTemplate.getForObject(url,String.class);
+        //3.利用@LoadBalanced注解的方式进行调用服务接口，可以在restTemplate里的url进行使用应用系统名称的方式进行调用
+        String response=restTemplate.getForObject("http://PRODUCT/msg",String.class);
+        log.info(response);
+        return response;
+    }
+}
--- src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
@@ -1,4 +1,4 @@
-package com.imooc.order.service;
+package com.imooc.order.service.impl;
 
 import com.imooc.order.dataobject.OrderDetail;
 import com.imooc.order.dataobject.OrderMaster;
@@ -7,17 +7,20 @@ import com.imooc.order.enums.OrderStatusEnum;
 import com.imooc.order.enums.PayStatusEnum;
 import com.imooc.order.repository.OrderDetailRepository;
 import com.imooc.order.repository.OrderMasterRepository;
+import com.imooc.order.service.OrderService;
 import com.imooc.order.utils.KeyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderDetailRepository orderDetailRepository;
 
@@ -28,13 +31,13 @@ public class OrderServiceImpl implements OrderService{
     public OrderDTO create(OrderDTO orderDTO) {
         String orderId = KeyUtil.genUniqueKey();
 
-        //查询商品信息(调用商品服务)
+        //TODO 查询商品信息(调用商品服务)
         List<String> productIdList = orderDTO.getOrderDetailList().stream()
                 .map(OrderDetail::getProductId)
                 .collect(Collectors.toList());
-        //调用商品服务查询商品信息Todo
+        //TODO 调用商品服务查询商品信息
 
-        //计算总价
+        //TODO 计算总价
 //        BigDecimal orderAmout = new BigDecimal(BigInteger.ZERO);
 //        for (OrderDetail orderDetail: orderDTO.getOrderDetailList()) {
 //            for (ProductInfoOutput productInfo: productInfoList) {
@@ -52,7 +55,7 @@ public class OrderServiceImpl implements OrderService{
 //            }
 //        }
 
-        //扣库存(调用商品服务)Todo
+        //扣库存(调用商品服务) TODO
 //        List<DecreaseStockInput> decreaseStockInputList = orderDTO.getOrderDetailList().stream()
 //                .map(e -> new DecreaseStockInput(e.getProductId(), e.getProductQuantity()))
 //                .collect(Collectors.toList());
@@ -62,9 +65,11 @@ public class OrderServiceImpl implements OrderService{
         OrderMaster orderMaster = new OrderMaster();
         orderDTO.setOrderId(orderId);
         BeanUtils.copyProperties(orderDTO, orderMaster);
-//        orderMaster.setOrderAmount(orderAmout);
-        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
-        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
+        orderMaster.setOrderAmount(new BigDecimal(5.0));
+        orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());//新订单
+        orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());//等待支付
+        orderMaster.setCreateTime(new Date());
+        orderMaster.setUpdateTime(new Date());
         orderMasterRepository.save(orderMaster);
         return orderDTO;
 
